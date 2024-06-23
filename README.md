d3f743c68bc939538b0ed216b99f84c9c08e5447
Vita <theprototype@Vitas-MBP.lan>
2024-06-23 11:19:33 -0500

    completed lab

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 
 ## Day III JavaScript Lab II 
 Guidelines 
-- [ ] Create an index.html file 
-- [ ] Include links to other pages necessary in your project
+- [x] Create an index.html file if necessary
+- [x] Include links to other pages necessary in your project
 - [x] Create an HTML file for your prompt choice 
 - [x] Create a compatible JS file for your prompt choice 
-- [ ] Test the coding for each page included in your project
+- [x] Test the coding for each page included in your project
 - [x] Create a CSS file to house the styling for your pages
-- [ ] Test each page in Live Server for accuracy in coding and functionality 
-- [ ] Sign as author 
-- [ ] Push final back to GitHub
+- [x] Test each page in Live Server for accuracy in coding and functionality 
+- [x] Sign as author 
+- [x] Push final back to GitHub
+
+# Author 
+- raivynbates@gmail.com
--- kindness.js
+++ kindness.js
@@ -4,12 +4,16 @@ function communicateKindness() {
     kindWords.observer = prompt("To whom, what or where are you sending this kindness to?")
     kindWords.message = prompt("Share your words of kindness here.");
   
-    pricelessKindness.innerHTML = `Greetings, ${kindWords.observer}! I hope this finds you at a time when you can cherish it most. ${kindWords.message}. I mean this sincerely. <br> Warmest regards, ${kindWords.curator}`
+    pricelessKindness.innerHTML = `Greetings, <em>${kindWords.observer}</em>!<br>
+    <br> I hope this finds you at a time when you can cherish it most. ${kindWords.message}. I mean this sincerely. <br>
+    <br>
+    <br> Warmest regards,<br><strong> ${kindWords.curator}</strong>`
   
-    someEncouragement.style.color = "hsl(300 50 50)";
+    someEncouragement.style.color = "#ffffff";
     someEncouragement.style.backgroundColor = "transparent";
-    pricelessKindness.style.color = "hsl (320 24 48)";
-    pricelessKindness.style.backgroundColor = "skyblue";
+    pricelessKindness.style.color = "#fffffff";
+    pricelessKindness.style.backgroundColor = "transparent";
+    
   }
   
   keepItKind.onclick = communicateKindness;
